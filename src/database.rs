3d913316f9d4fa8ed51f9c7f31e827f9d73d4df0
Ɓʘɓƴ <shboby71@gmail.com>
2024-11-23 22:15:59 +0530

    minor Changes

--- src/database.rs
+++ src/database.rs
@@ -1,5 +1,4 @@
-use sqlx::mysql::MySqlPool;
+use sqlx::MySqlPool;
 pub async fn database_connection() -> Result<MySqlPool, sqlx::Error> {
-    let connection_string = "mysql://root:root@127.0.0.1:3307/actix-web";
-    MySqlPool::connect(connection_string).await
+    MySqlPool::connect("mysql://root:root@localhost:3306/actix-web").await
 }
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,21 @@
-use actix_web::{web, App, HttpServer};
+use actix_web::{App, HttpServer};
 mod database;
 mod routes;
 use database::*;
 use routes::*;
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let db = database_connection()
-        .await
-        .expect("Failed to connect to the database");
+    let database = database_connection().await.expect("Faild To Connect");
     println!("Database connected");
-    let db_data = web::Data::new(db);
     let server = HttpServer::new(move || {
         App::new()
-            .app_data(db_data.clone())
+            .app_data(database.clone())
             .service(home)
             .service(hello_user)
             .service(create_new_user)
     })
-    .bind(("127.0.0.1", 8000))? // Port 8000 for the HTTP service
+    .bind(("localhost", 8080))?
     .run();
-
-    println!("Service is working at http://127.0.0.1:8000/home");
+    println!("Service is working at localhost:8080");
     server.await
 }
