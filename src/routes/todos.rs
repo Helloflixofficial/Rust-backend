375a768979cdb7f0f1370bd3ce72189be1cc4055
Ɓʘɓƴ <shboby71@gmail.com>
2024-12-06 22:40:30 +0530

    new Stuf for me again

--- src/routes/todos.rs
+++ src/routes/todos.rs
@@ -1,8 +1,9 @@
 use actix_web::web::{Data, Json};
-use actix_web::{post, HttpResponse, Responder};
+use actix_web::{get, post, HttpResponse, Responder};
 use serde::{Deserialize, Serialize};
-use sqlx::{mysql::MySql, Error, FromRow};
-
+use sqlx::MySqlPool;
+use sqlx::{mysql::MySqlQueryResult, Error, FromRow};
+#[derive(Deserialize)]
 pub struct CreateNewTodos {
     pub title: String,
     pub description: Option<String>,
@@ -16,7 +17,7 @@ pub struct TODO {
     pub status: String,
 }
 
-#[derive(Serialize)]
+#[derive(Serialize, Deserialize)]
 pub struct TypeDBError {
     pub error: String,
 }
@@ -26,11 +27,12 @@ pub async fn create_new_todos(
     db: Data<sqlx::MySqlPool>,
     body: Json<CreateNewTodos>,
 ) -> impl Responder {
-    let response = sqlx::query("INSERT INTO TODOS (title, description) VALUES (?, ?)")
-        .bind(&body.title)
-        .bind(&body.description)
-        .execute(&**db)
-        .await;
+    let response: Result<MySqlQueryResult, Error> =
+        sqlx::query("INSERT INTO TODOS (title, description) VALUES (?, ?)")
+            .bind(&body.title)
+            .bind(&body.description)
+            .execute(&**db)
+            .await;
 
     match response {
         Ok(result) => HttpResponse::Created().json(TODO {
@@ -39,6 +41,21 @@ pub async fn create_new_todos(
             description: body.description.clone(),
             status: "new".to_string(),
         }),
+        Err(e) => HttpResponse::InternalServerError().json(TypeDBError {
+            error: e.to_string(),
+        }),
+    }
+}
+
+#[get("/todos/all")]
+pub async fn get_all_todos(db: Data<MySqlPool>) -> impl Responder {
+    let res: Result<Vec<TODO>, Error> =
+        sqlx::query_as("SELECT id, title, description, status FROM todos")
+            .fetch_all(&**db)
+            .await;
+
+    match res {
+        Ok(todos) => HttpResponse::Ok().json(todos),
         Err(e) => HttpResponse::InternalServerError().json(TypeDBError {
             error: e.to_string(),
         }),
